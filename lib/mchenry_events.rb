0fe0825e1a3f7fc338d4aafaa7268649a408e15a
mmccarthy64 <mmccarthy64@hotmail.com>
2020-09-22 12:09:19 -0500

    deleted unnecessary files

--- lib/mchenry_events.rb
+++
@@ -1,9 +0,0 @@
-require_relative "./mchenry_events/version"
-require_relative "./mchenry_events/cli"
-
-require 'pry'
-
-module MchenryEvents
-  class Error < StandardError; end
-  # Your code goes here...
-end
