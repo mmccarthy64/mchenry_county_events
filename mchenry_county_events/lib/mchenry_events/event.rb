b44762d03eed9155e21d089a155600ea11ee0946
mmccarthy64 <mmccarthy64@hotmail.com>
2020-09-24 14:07:18 -0500

    done

---
+++ mchenry_county_events/lib/mchenry_events/event.rb
@@ -0,0 +1,65 @@
+class MchenryEvents::Event
+  attr_accessor :name, :city
+  
+  @@all = []
+  
+  def initialize(name, city)
+    @name = name
+    @city = city
+    add_to_city
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_to_city
+    @city.events << self unless @city.events.include?(self)
+  end
+  
+  
+  
+  
+  def get_event_info
+    # binding.pry
+    MchenryEvents::Scraper.scrape_event_info(self) if @events.empty?
+    @events
+  end
+  
+  
+  
+
+class MchenryEvents::Event
+  attr_accessor :name, :city
+  
+  @@all = []
+  
+  def initialize(name, city)
+    @name = name
+    @city = city
+    add_to_city
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_to_city
+    @city.events << self unless @city.events.include?(self)
+  end
+
+  def self.sort_by_name
+    self.all.sort {|a,b| a.name <=> b.name}
+  end
+
+end
