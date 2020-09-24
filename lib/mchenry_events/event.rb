25e6ee866c0fd5945e9e6e5eae0feef86bdc0fbb
mmccarthy64 <mmccarthy64@hotmail.com>
2020-09-24 15:57:19 -0500

    error

--- lib/mchenry_events/event.rb
+++ lib/mchenry_events/event.rb
@@ -1,37 +1,37 @@
 class MchenryEvents::Event
-  attr_accessor :name, :city
+    attr_accessor :name, :city
+    
+    @@all = []
+    
+    def initialize(name, city)
+      @name = name
+      @city = city
+      add_to_city
+      save
+    end
+    
+    def save
+      @@all << self
+    end
+    
+    def self.all
+      @@all
+    end
+    
+    def add_to_city
+      @city.events << self unless @city.events.include?(self)
+    end
+    
+    
+    
+    
+    def get_event_info
+      # binding.pry
+      MchenryEvents::Scraper.scrape_event_info(self) if @events.empty?
+      @events
+    end
+    
+    
+    
   
-  @@all = []
-  
-  def initialize(name, city)
-    @name = name
-    @city = city
-    add_to_city
-    save
   end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def add_to_city
-    @city.events << self unless @city.events.include?(self)
-  end
-  
-  
-  
-  
-  def get_event_info
-    # binding.pry
-    MchenryEvents::Scraper.scrape_event_info(self) if @events.empty?
-    @events
-  end
-  
-  
-  
-
-end
--- lib/mchenry_events/scraper.rb
+++ lib/mchenry_events/scraper.rb
@@ -1,29 +1,29 @@
 class MchenryEvents::Scraper
   
-  def self.scrape_cities
-    doc = Nokogiri::HTML(open("https://www.visitmchenrycounty.com/Annual-Events"))
-    
-    cities = doc.css('select#city option').reverse
-    
-    cities.each do |n|
-      name = n.text
-      MchenryEvents::City.new(name)
-    end
-  end
-  
-  def self.scrape_events(city)
-    doc = Nokogiri::HTML(open("https://www.visitmchenrycounty.com/Annual-Events"))
-    
-    name_of_event = doc.css('div.listingWrap div.listingContent')
-    
-    city = city.name
-    
-    name_of_event.collect do |event, index|
-      if event.text.include?(city)
-        name = event.css('h4').text
-        puts "#{index}. #{name}"
+    def self.scrape_cities
+      doc = Nokogiri::HTML(open("https://www.visitmchenrycounty.com/Annual-Events"))
+      
+      cities = doc.css('select#city option').reverse
+      
+      cities.each do |n|
+        name = n.text
+        MchenryEvents::City.new(name)
       end
     end
-  end
     
-end
+    def self.scrape_events(city)
+      doc = Nokogiri::HTML(open("https://www.visitmchenrycounty.com/Annual-Events"))
+      
+      name_of_event = doc.css('div.listingWrap div.listingContent')
+      
+      city = city.name
+      
+      name_of_event.collect do |event, index|
+        if event.text.include?(city)
+          name = event.css('h4').text
+          puts "#{index}. #{name}"
+        end
+      end
+    end
+      
+  end
